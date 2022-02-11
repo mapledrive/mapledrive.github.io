48201f93d971e0cb847e0da858aa549908d0eb7f
Sait <sharifullin_sa@astral.ru>
2022-02-11 23:19:08 +0500

    absolute paths

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import { Header } from './components/Header';
-import { Footer } from './components/Footer';
-import { Main } from './components/Main';
-import { Navigation } from './components/Navigation';
-import { Bottom } from './components/Bottom';
+import 'index.css';
+import { Header } from 'components/Header';
+import { Footer } from 'components/Footer';
+import { Main } from 'components/Main';
+import { Navigation } from 'components/Navigation';
+import { Bottom } from 'components/Bottom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 

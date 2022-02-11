f385f7788529d47e85830e3ee072461beb0e5aa9
Sait <sharifullin_sa@astral.ru>
2022-02-12 00:47:22 +0500

    dates

--- src/components/Bottom.js
+++ src/components/Bottom.js
@@ -1,3 +1,5 @@
-import React from 'react'
+import React from 'react';
 
-export const Bottom = () => <div className="bottom">©2009-2019 Maple Drive. All rights reserved.</div>;
+export const Bottom = () => (
+  <div className='bottom'>©2019-2022 Maple Drive. All rights reserved.</div>
+);

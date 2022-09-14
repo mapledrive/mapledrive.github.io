fc5d7e34d650a3381d5c8c1ada3bfa9a51d97f81
mapledrive <fastmn@yahoo.com>
2022-09-14 23:50:08 +0500

    route

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,12 +11,15 @@ export const Main = () => (
   <main>
     <Routes>
       <Route path='/' element={<Homepage />} />
-      <Route path='/home' element={<Homepage />} />
       <Route path='/service' element={<Servicepage />} />
       <Route path='/delivery' element={<Deliverypage />} />
       <Route path='/slider' element={<Sliderpage />} />
       <Route path='/currency' element={<Currencypage />} />
       <Route path='/information' element={<Informationpage />} />
+      <Route path='*' element={<Homepage />} />
     </Routes>
   </main>
 );
+
+// Using path="*"" means "match anything", so this route acts
+// like a catch-all for URLs that we don't have explicit routes for.

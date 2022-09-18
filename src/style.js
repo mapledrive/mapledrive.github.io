c92bf34ad8af548cd0041f1998c0192e76b9178e
mapledrive <sait.sharifullin@gmail.com>
2022-09-18 20:36:34 +0500

    overlay

--- src/style.js
+++ src/style.js
@@ -338,7 +338,7 @@ export const Overlay = styled.div`
   align-items: center;
   justify-content: center;
   width: 100%;
-  height: 300px;
+  height: 400px;
   position: absolute;
   top: 0;
   left: 0;

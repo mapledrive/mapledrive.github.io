92fab74364f44f67006d26a05ffde69c6691b894
mapledrive <sait.sharifullin@gmail.com>
2022-10-10 00:17:53 +0500

    about page populated

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,17 +1,29 @@
 import { StyledSection, SectionTitle, SectionContent } from 'style';
+import styled from 'styled-components';
 
 const Homepage = () => {
   return (
     <StyledSection>
-      <SectionTitle>Hello, this is Sait Sharifullin</SectionTitle>
-      <SectionContent>
-        I am working as a Frontend Developer. I love to code and make new web
-        experience for the people.
-        <br />
-        That is my personal website, where you will find some of my works
-      </SectionContent>
+      <SectionTitle>Hi, I'm Sait 👋</SectionTitle>
+      <SectionTitle>I love to build amazing apps.</SectionTitle>
+      <SkillsContent>
+        I am a Javascript Developer 🚀 I have experience in building <br />
+        Web applications with JavaScript / Reactjs / Redux / Material UI <br />
+        and some other cool libraries and frameworks.
+      </SkillsContent>
     </StyledSection>
   );
 };
 
 export { Homepage };
+
+export const SkillsContent = styled.main`
+  color: #222;
+  padding: 12px 0px;
+  box-sizing: border-box;
+  font-size: 20px;
+  font-family: Roboto;
+  line-height: 30px;
+  font-weight: 400;
+  font-style: normal;
+`;

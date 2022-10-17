3f45719374df60086edc50fb4f6d0091ed90ae4c
mapledrive <sait.sharifullin@gmail.com>
2022-10-18 01:54:34 +0500

    New commit for maple

--- src/features/news/newsAPI.js
+++ src/features/news/newsAPI.js
@@ -4,5 +4,6 @@ export const fetchNewsApi = async () => {
   const response = await axios.get(
     `https://inshortsapi.vercel.app/news?category=technology`
   );
+  console.log('response', response);
   return response.data.data;
 };

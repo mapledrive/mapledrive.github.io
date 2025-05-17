71c97648a586f641b21de38ea63f859fde6ca476
mapledrive <sait.sharifullin@gmail.com>
2025-05-17 18:17:24 +0500

    year

--- src/components/Bottom.js
+++ src/components/Bottom.js
@@ -2,5 +2,5 @@ import React from 'react';
 import { StyledBottom } from 'style';
 
 export const Bottom = () => (
-  <StyledBottom>©2019-2022 Maple Drive. All rights reserved.</StyledBottom>
+  <StyledBottom>©2019-2025 Maple Drive. All rights reserved.</StyledBottom>
 );

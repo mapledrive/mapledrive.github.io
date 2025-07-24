324eeeab69dcd15c8659033a829ae8b7deb802da
mapledrive <sait.sharifullin@gmail.com>
2025-07-25 00:00:50 +0500

    paths

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'development' ? '/' : '/mapledrive.github.io/',
+  base: '/',
   plugins: [react()],
   build: {
     outDir: 'dist', // совпадает с React Scripts, если хотите сохранить папку билда

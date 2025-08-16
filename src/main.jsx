f1742c62a0e4a66ae272fa2daef5c3a287d79dc7
mapledrive <sait.sharifullin@gmail.com>
2025-08-16 15:13:36 +0500

    vgfegwe

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <BrowserRouter>
+    <p>tested</p>
+  </BrowserRouter>
+);
--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
-export default defineConfig(({ mode }) => ({
-  base: '/',
-  plugins: [react()],
-  build: {
-    outDir: 'dist', // совпадает с React Scripts, если хотите сохранить папку билда
-  },
-  resolve: {
-    alias: {
-      app: path.resolve(__dirname, 'src/app'), // для alias 'app'
-      features: path.resolve(__dirname, 'src/features'), // для alias 'features'
-      components: path.resolve(__dirname, 'src/components'),
-      fonts: path.resolve(__dirname, 'src/fonts'),
-      hooks: path.resolve(__dirname, 'src/hooks'),
-      pages: path.resolve(__dirname, 'src/pages'),
-      sagas: path.resolve(__dirname, 'src/sagas'),
-      utils: path.resolve(__dirname, 'src/utils'),
-      style: path.resolve(__dirname, 'src/style.jsx'),
+export default defineConfig(() => {
+  return {
+    build: {
+      outDir: 'build',
     },
-  },
-  server: {
-    open: true,
-    port: 3000,
-  },
-}));
+    plugins: [react()],
+  };
+});

7d0b1bfa4965c685bbed4326ef992c13f372597f
mapledrive <sait.sharifullin@gmail.com>
2025-08-16 15:35:37 +0500

    праправкаеуы

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(() => {
-  return {
-    build: {
-      outDir: 'build',
-    },
-    plugins: [react()],
-  };
+// https://vitejs.dev/config/
+// import.meta.env.BASE_URL === /recognition/
+export default defineConfig({
+  base: '/',
+  plugins: [react()],
+  server: {
+    open: true,
+    port: 3000,
+  },
 });

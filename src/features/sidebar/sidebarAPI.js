16abbd2362d12ca365ee6d24d0bf21e329f957e6
mapledrive <sait.sharifullin@gmail.com>
2025-10-12 01:50:58 +0500

    API mistake

--- src/features/sidebar/sidebarAPI.js
+++ src/features/sidebar/sidebarAPI.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 export const fetchSidebarApi = async () => {
+  if (false) {
+    const response = await axios.get(
+      `https://inshortsapi.vercel.app/news?category=national`
+    );
+    return response.data.data;
+  }
   throw new Error('API request blocked intentionally');
-  // The line below will never execute
-  const response = await axios.get(
-    `https://inshortsapi.vercel.app/news?category=national`
-  );
-  return response.data.data;
 };

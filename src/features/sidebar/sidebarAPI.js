97f10e35e4af8c1a4646d55638b9f20716be10f0
mapledrive <sait.sharifullin@gmail.com>
2025-10-12 01:27:03 +0500

    temporary suspension of sidebar API call

--- src/features/sidebar/sidebarAPI.js
+++ src/features/sidebar/sidebarAPI.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
 export const fetchSidebarApi = async () => {
+  throw new Error('API request blocked intentionally');
+  // The line below will never execute
   const response = await axios.get(
     `https://inshortsapi.vercel.app/news?category=national`
   );

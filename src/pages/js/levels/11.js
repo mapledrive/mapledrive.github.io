e764cea651e2cc79c66b24d5ff896232edf5c63f
mapledrive <sait.sharifullin@gmail.com>
2025-10-25 21:44:34 +0500

    great

--- src/pages/js/levels/11.js
+++ src/pages/js/levels/11.js
@@ -3,7 +3,6 @@ import { Level } from './level';
 import { gameState } from '../gameState';
 
 export function oneone() {
-  // Создаем уровень и сохраняем в gameState
   gameState.level = new Level({
     playerPos: [56, 192],
     loader: oneone,
@@ -19,20 +18,15 @@ export function oneone() {
     [13, 212],
   ];
 
-  // Инициализируем игровые переменные в gameState
   gameState.vX = 0;
   gameState.vY = 0;
 
-  // Устанавливаем позицию игрока
   if (gameState.player) {
     gameState.player.pos[0] = gameState.level.playerPos[0];
     gameState.player.pos[1] = gameState.level.playerPos[1];
   }
 
-  // Строим землю
   ground.forEach(function (loc) {
     gameState.level.putFloor(loc[0], loc[1]);
   });
-
-  // Функция ничего не возвращает, работает только с gameState
 }
